49c4fc927c9bb2634a33f4c0ddf5028f3360fcdf
Alina CORNEA <alcornea@e1z2r9p9.42.us.org>
2017-08-05 13:44:29 -0700

    adding

--- admin/shared/footer.php
+++ admin/shared/footer.php
@@ -1,6 +1,6 @@
-<footer>
+<!-- <footer>
   &copy; <?php echo date('Y'); ?> Vintage
 </footer>
 </body>
 
-</html>
+</html> -->
--- public/staff/subjects/show_product.php
+++ public/staff/subjects/show_product.php
@@ -1,5 +1,5 @@
 <?php
 $id = isset($_GET['id']) ? $_GET['id']: '1';
 
-echo $id;
+echo h($id);
 ?>
