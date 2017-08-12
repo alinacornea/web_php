c4149a2ee1760554adeb71eecbed38722ed3d56e
Alina CORNEA <alcornea@e1z3r9p4.42.us.org>
2017-08-11 21:59:21 -0700

    add

--- public/index.php
+++ public/index.php
@@ -1,28 +1,73 @@
 <?php  session_start(); ?>
 <?php require_once('../shared/initialize.php'); ?>
-<?php include('../admin/headers/header.php'); ?>
+<!doctype html>
 
-<style>
-body {
-    background-repeat: repeat;
-    background-image: url(../resources/bg3.jpg);
-    font-family: "Book Antiqua", Palatino, serif;
-    font-size-adjust: auto;
-}
-</style>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Vintage - store </title>
+  <link rel="icon" href="images/favicon.png"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet"
+     href="https://fonts.googleapis.com/css?family=Tangerine">
+</head>
+
+<body>
+    <header>
+      <h1>
+        <a title="Home"href="<?php echo ("http://localhost:8080/ecommerce/public/index.php?login=$login") ;?>"> <img id="home" src="images/home.png" alt="Home"></a>
+        Vintage Store
+        <a title="Cart" href="buy_product.php"> <img id="cart" src="images/cart.png" alt="Cart"></a>
+        <a title="Admin" target="_blank" href="../admin/users/admin_login.php"><img id="admin" src="images/admin.png" alt="Admin"></a>
+        <a title="Logout" href="../admin/users/logout.php"><img id="logout" src="images/logout.png" alt="Logout"></a>
+        <a title="Login" href="../admin/users/login.php"> <img id="login"src="images/login.png" alt="Login"></a>
+      </h1>
+   <style>
+     h1 {
+       text-align: center;
+       margin: 0 0 10px 0;
+       background: #FFDAB9;
+       font-family: 'Tangerine', serif;
+       font-size: 60px;
+     }
+     #home{
+      float: left;
+      padding: 5px;
+      width:50px;
+      height:50px;
+     }
+     #cart{
+       float: right;
+       padding: 10px;
+       width:40px;
+       height:40px;
+     }
+     #admin{
+       float: right;
+       padding: 10px;
+       width:40px;
+       height:40px;
+     }
+     #logout{
+       float: right;
+       padding: 10px;
+       width:40px;
+       height:40px;
+     }
+     #login{
+       float: right;
+       padding: 10px;
+       width:40px;
+       height:40px;
+     }
+   </style>
+    </header>
+
 <div id="content">
   <div id="main-menu">
     <ul>
-      <li> <img src="images/login.png"  alt="Login" style="width:40px;height:40px;">
-        <a href="../admin/users/login.php"> Login Account </a></li>
-        <li>
-          <img src="images/logout.png" alt="Logout" style="width:40px;height:40px;">
-          <a href="../admin/users/logout.php"> Logout </a></li>
-      <li> <img src="images/admin.png" alt="Admin" style="width:40px;height:40px;">
-        <a target="_blank" href="../admin/users/admin_login.php"> Admin Area </a></li>
-      <li> <img src="images/cart.png" alt="Cart" style="width:40px;height:40px;">
-        <a href="buy_product.php"> Shoping cart </a>
-      </li>
+
     </ul>
   </div>
 
