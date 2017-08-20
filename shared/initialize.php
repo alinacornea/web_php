c9308dc571f54847578a2a75ad0890d459b9c3ad
Alina CORNEA <alcornea@e1z3r9p4.42.us.org>
2017-08-19 22:33:03 -0700

    database

--- shared/initialize.php
+++ shared/initialize.php
@@ -1,4 +1,5 @@
 <?php
+require_once('functions.php');
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/admin') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define ("WWW_ROOT", $doc_root);
@@ -7,5 +8,12 @@ $public = strpos($_SERVER['SCRIPT_NAME'], '/public') + 20;
 $root = substr($_SERVER['SCRIPT_NAME'], 0, $public);
 define ("WWW_PUBLIC", $root);
 
-require_once('functions.php');
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "web_store";
+global $conn;
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (mysqli_connect_errno())
+  echo "Failed to connect to MySQL: " . mysql_connect_error();
 ?>
